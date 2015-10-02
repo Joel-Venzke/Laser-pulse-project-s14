955eaef084299f0f020e88d6fc194569d2e25363
Joel-Venzke <joel.venzke@drake.edu>
2015-10-01 19:10:43 -0500

    setting up the massive IR run

--- REU-Files/Job-scripts/name_jobs.sh
+++ REU-Files/Job-scripts/name_jobs.sh
@@ -1,5 +1,8 @@
-for i in $( ls 3pulse_rr2*.sh); do 
-	echo $i;
-	i2=$(echo $i | sed 's/3pulse_rr2/3pulse_rr2_ir/g')
-	cp $i $i2
+PAR=( 0.701d0 0.702d0 0.703d0 0.704d0 0.705d0 0.706d0 0.707d0 0.708d0 0.709d0 0.71d0 0.711d0 0.712d0 0.713d0 0.714d0 0.715d0 0.716d0 0.717d0 0.718d0 0.719d0 0.72d0 0.721d0 0.722d0 0.723d0 0.724d0 0.725d0 0.726d0 0.727d0 0.728d0 )
+
+for i in ${PAR[*]}; do 
+	# i2=$(echo $i | sed 's/3pulse_rr2/3pulse_rr2_ir/g')
+	# cp 3pulse_rr2_ir_0.700d0.sh 3pulse_rr2_ir_$i.sh
+	# subl 3pulse_rr2_ir_$i.sh
+	# rm 3pulse_rr2_ir_$i.sh
 done
--- REU-Files/Job-scripts/submit_jobs.sh
+++ REU-Files/Job-scripts/submit_jobs.sh
@@ -1,4 +1,13 @@
-for i in $( ls 3pulse_rr2_ir*.sh); do 
-	echo $i;
-	sbatch $i;
+# for i in $( ls 3pulse_rr2_ir*.sh); do 
+# 	echo $i;
+# 	sbatch $i;
+# done
+
+PAR=( 0.701d0 0.702d0 0.703d0 0.704d0 0.706d0 0.707d0 0.708d0 0.709d0 0.711d0 0.712d0 0.713d0 0.714d0 0.716d0 0.717d0 0.718d0 0.719d0 0.721d0 0.722d0 0.723d0 0.724d0 0.726d0 0.727d0 )
+
+for i in ${PAR[*]}; do 
+	echo ============================= 3pulse_rr2_ir_$i.sh =====================================
+	# sbatch 3pulse_rr2_ir_$i.sh
+	# diff 3pulse_rr2_ir_$i.sh 3pulse_rr2_ir_0.700d0.sh
+	# read -p "$*"
 done
